0f0aa6b2148195d8aef37411aaf451d9acf6fb3a
Zachary Wartell <zjayuu@gamil.com>
2026-09-03 20:25:52 -0400

    featureupdate

--- .zxw-env/git-ssh.sh
+++ .zxw-env/git-ssh.sh
@@ -1,3 +1,27 @@
+# Version 1.1
 eval $(ssh-agent -s)
-read -p "ssh key file name: " KEY_FILE
-ssh-add ~/.ssh/ids/$KEY_FILE
+hostname=$(uname -a | awk '{print $2}')
+remote=$(git remote -v)
+if [[ $remote =~ "github" ]]; then
+    target="github"
+fi
+if [[ $remote =~ "gitlab" ]]; then
+    target="gitlab"
+fi
+home=$(dirname ~/.ssh)
+guess="$home/.ssh/ids/$hostname-to-$target"
+echo guess: $guess
+if [[ -f $guess ]]; then    
+    read -p "Use ssh key file name '$guess'? [y/n]" yn
+    if [[ $yn -eq "y" ]]; then
+        ssh-add $guess
+    else
+        read -p "ssh key file name: " KEY_FILE
+        ssh-add ~/.ssh/ids/$KEY_FILE
+    fi
+else
+    read -p "ssh key file name: " KEY_FILE
+    ssh-add ~/.ssh/ids/$KEY_FILE
+fi
+
+
